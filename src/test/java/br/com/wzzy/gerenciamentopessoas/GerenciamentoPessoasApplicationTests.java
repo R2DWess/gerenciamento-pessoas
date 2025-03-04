176670b0f6351d441a76018fde3a7bcba55da7cf
R2DWess <gusterlima@hotmail.com>
2025-03-04 11:51:41 -0300

    primeiro commit

---
+++ src/test/java/br/com/wzzy/gerenciamentopessoas/GerenciamentoPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wzzy.gerenciamentopessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
